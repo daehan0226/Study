8583d9187d2c3f9004995c8404ac96960f63ad6e
Foxlee1015 <47915302+Foxlee1015@users.noreply.github.com>
2019-07-10 00:32:13 +0900

    Update 2019-07-10-Nodejs_mysql.js

--- _posts/JS/2019-07-10-Nodejs_mysql.js
+++ _posts/JS/2019-07-10-Nodejs_mysql.js
@@ -5,3 +5,40 @@ categories: JS
 
 ---
 
+ 
+  
+
+        var express = require('express');
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host :'localhost',                     // ip address
+            port : '3306',
+            user : 'root',
+            password : '50174397',
+            database : 'users'
+        });
+
+
+        var app = express();
+
+        app.set('port', process.env.PORT || 3000);
+
+        app.get('/', function(req, res){
+          res.send('Root');
+        });
+
+        app.get('/board', function(req, res){
+
+          connection.query('SELECT * from board', function(err, rows) {
+            if(err) throw err;
+
+            console.log('board information : ', rows);
+            res.send(rows);
+          });
+        });
+
+        app.listen(app.get('port'), function () {
+          console.log('Express server listening on port ' + app.get('port'));
+        });
+
+
